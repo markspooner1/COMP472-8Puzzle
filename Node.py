bee297db07814dbe67b744edf6233e31a2298605
markspooner1 <markaustinspooner@gmail.com>
2023-02-26 19:27:09 -0500

    Added heuristics

--- Node.py
+++ Node.py
@@ -9,79 +9,61 @@ Max children is 4
     - as a list: [1, 2, 3, 8, B, 4, 7, 6, 5]
  """
  # Will need to set root parent to null
-goal = [1, 2, 3, 8, 'B', 4, 7, 6, 5]
+coordinates = {0:[0,0], 1:[1,0], 2:[2,0],
+               3:[0,1], 4:[1,1], 5:[2,1],
+               6:[0,2], 7:[1,2], 8:[2,2]}
+goalcoordinates = {'B':[1,1], 1:[0,0], 2:[1,0],
+               3:[2,0], 4:[2,1], 5:[2,2],
+               6:[1,2], 7:[0,2], 8:[0,1]}
+
+goal = [1,2,3,8,'B',4,7,6,5]
 cols = 3
 class Node:
-    def __init__(self, parent, state, depth, pathCost):
+    def __init__(self, parent, state, depth):
         self.children = []
         self.parent = parent
         self.state = state
         self.depth = depth
-        self.pathCost = pathCost
+        self.pathCost = self.permutationInversion()
     def isGoal(self):
        return self.state == goal
     def moveRight(self, index):
-        # mod by number of cols to make sure move right is valid
-        # if valid, generate puzzle
-        # get num to right of blank, swap them
         if index % cols < 2:
-            
-           
-            # todo -> check if deep copy
             childState = self.state.copy()
             childState[index], childState[index + 1]  =  self.state[index + 1], 'B'
-            # TODO Path Cost
-              # add node to children of self
-             # set parent of new node to self
-            child = Node(self, childState, self.depth + 1, 0)
+            child = Node(self, childState, self.depth + 1)
             self.children.append(child)
     def moveLeft(self, index):
-        
-        # similar idea for move right
-        # can only move left if the index mod cols is greater than 0
-        # follow same algo for move Right
         if index % cols > 0:
-     
             tempLeftNum = self.state[index - 1]
-            # todo -> check if deep copy
             childState = self.state.copy()
             childState[index] = tempLeftNum
             childState[index - 1] = 'B' 
-            # TODO Path Cost
-              # add node to children of self
-             # set parent of new node to self
-            child = Node(self, childState, self.depth + 1, 0)
+            child = Node(self, childState, self.depth + 1)
             self.children.append(child)
     def moveUp(self, index):
-        # check if index - cols >= 0
         if index - cols >= 0:
-
             tempUpNum = self.state[index - 3]
             childState = self.state.copy()
             childState[index] = tempUpNum
             childState[index - 3] = 'B'
-
-            child = Node(self, childState, self.depth + 1, 0)
+            child = Node(self, childState, self.depth + 1)
             self.children.append(child)
     def moveDown(self, index):
         # check if i + col < 9
         if index + cols < 9:
-        
             tempNumDown = self.state[index + 3]
             childState = self.state.copy()
             childState[index] = tempNumDown
             childState[index + 3] = 'B'
-
-            child = Node(self, childState, self.depth + 1, 0)
+            child = Node(self, childState, self.depth + 1)
             self.children.append(child)
-    
     def generateSuccessors(self):
         index = self.state.index('B')
         self.moveRight(index)
         self.moveLeft(index)
         self.moveUp(index)
         self.moveDown(index)
-
     def printPuzzle(self):
         print("\n")
         for i in range(3):
@@ -95,9 +77,50 @@ class Node:
             row += '|'
             print(row)
         print("\n")
+
     def hammingDistance(self):
-        pass
-    
+        # Count the number of tiles out of place, dont count the blank tile
+        hammingDistance = 0
+        for i, val in enumerate(self.state):
+            currPos = coordinates[i]
+            goalPos = goalcoordinates[val]
+            if currPos != goalPos and val != 'B':
+                hammingDistance+=1
+        return hammingDistance
+
+    def manhattanDistance(self):
+        manDistance = 0
+        for i, val in enumerate(self.state):
+            currentPosX, currentPosY = coordinates[i]
+            goalPosX, goalPosY = goalcoordinates[val]
+            manDistance += abs(currentPosY - goalPosY) + abs(currentPosX - goalPosX)     
+        return manDistance
+    def permutationInversion(state):
+        #for each value in state
+        #for each value to its right
+            #if when we check the goal state, value2 is actually supposed to be to the left of value, we increment 
+            # if index of value2 in goal state is < current index of value in goal state increment++ 
+        sum = 0
+        for i, val in enumerate(state):
+            if val == 'B':
+                continue
+            for j in range(i + 1, len(state)):
+                if val == 'B':
+                    continue
+                index1 = goal.index(val)
+                index2 = goal.index(state[j])
+                if index2 < index1:
+                    sum+=1
+        return sum
+                
+
+
+    def fFunctionHeuristic(self):
+        return self.manhattanDistance() + self.depth
+
+
+
+
         
         
 
--- Search.py
+++ Search.py
@@ -3,9 +3,13 @@
 
 from Node import *
 from collections import deque
-start = [1, 2, 3, 7, 8, 4, 'B', 6, 5]
+start = [1,2,3,4,5,6,7,'B',8]
+# 1 2 3
+# 8   4  
+# 7 6 5
+root = Node(None, start, 0)
+goal = [1,2,3,8,'B',4,7,6,5]
 
-root = Node(None, start, 0, 0)
 def DFS():
     
     # root.printPuzzle()
@@ -15,11 +19,9 @@ def DFS():
     open = [root]
     closed = []
     while(len(open) > 0):
-        x = open.pop()
-       # x.printPuzzle()
+        x = open.pop()  
         
         if x.isGoal():
-            
             return "Goal"
         else:
             x.generateSuccessors()
@@ -32,7 +34,6 @@ def BFS():
     closed = []
     while(len(open) > 0):
         x = open.popleft()
-        x.printPuzzle()
         if x.isGoal():
             x.printPuzzle()
             return 'Goal'
@@ -43,16 +44,52 @@ def BFS():
                 if not isOnOpenOrClosed(open, closed, child.state):
                     open.append(child)
     return "Fail"
+
 def BestFS():
-    pass
+    open = deque([root])
+    closed = []
+    while(len(open) > 0):
+        x = open.popleft()
+        closed.append(x)
+        if x.isGoal():
+            return 'Goal'
+        else:
+            x.generateSuccessors()
+            for child in x.children:
+                if not isOnOpenOrClosed(open, closed, child.state):
+                    open.append(child)
+            sorted(open, key=lambda x: x.pathCost)
+
+def AStar():
+    open = deque([root])
+    closed = []
+    while(len(open) > 0):
+        x = open.popleft()
+        if x.isGoal():
+            return "Goal"
+        else:
+            closed.append(x)
+            x.generateSuccessors()
+            for child in x.children:
+                if not isOnOpenOrClosed(open, closed, child.state):
+                    open.append(child)
+            sorted(open, key=lambda x: x.pathCost)
 
     
 def isOnOpenOrClosed(open, closed, puzzle):
-   return puzzle in open or puzzle in closed
-
-
+    res = False
+    for node in open:
+       if node.state == puzzle:
+           res = True
+    for node in closed:
+        if node.state == puzzle:
+            res= True
+    return res
 
 if __name__ == "__main__":
-    DFS()
-    #print(BFS())
 
+    pass
+    #DFS()
+    #print(BFS())
+    #print(BestFS())
+    #print(AStar())
