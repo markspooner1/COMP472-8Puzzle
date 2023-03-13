7dafe553cc4d0dcdb42fe6215f0aa3ca3273d0af
markspooner1 <markaustinspooner@gmail.com>
2023-03-12 22:09:47 -0400

    add args parser

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 from Search import *
-
+import argparse
+parser = argparse.ArgumentParser(description="Enter puzzle, search type, and heuristic")
+parser.add_argument("puzzle",type=list ,help="Enter puzzle as a string of numbers ex: 2831647B5")
+parser.add_argument("type", type=int,help="Enter search type: 1 -> Depth First Search, 2 -> Breadth First Search, 3 -> Best First Search, 4 -> A Star Search")
+parser.add_argument("heuristic", type=str,help="Enter Heuristic: HD -> Hamming Distance, MD -> Manhattan Distance, PI -> Permutation Inversions, A1 -> My Custom heuristic from Theory Assignment 1 (linear conflict + manhattan distance), IH -> Inadmissable Heuristic")
+args = parser.parse_args()
 if __name__ == "__main__":
-    start = [5, 1, 4, 7, 'B', 6, 3, 8, 2]
-    type = input(
-    """Enter search: 
-        1 -> Depth First Search
-        2 -> Breadth First Search
-        3 -> Best First Search
-        4 -> A Star Search
-    (Enter either 1 2 3 or 4): """)
-    if int(type) == 3 or int(type) == 4:
-        heuristic = input("""
-        Enter Heuristic:
-            HD -> Hamming Distance 
-            MD -> Manhattan Distance 
-            PI -> Permutation Inversions
-            A1 -> My Custom heuristic from Theory Assignment 1 (linear conflict + manhattan distance)
-            IH -> Inadmissable Heuristic
-        (Enter either HD, MD, PI, A1, or IH): """)
-    else: 
-        heuristic = None
-    search = Search(type, start, heuristic)
+    start = args.puzzle
+    type = args.type
+    heuristic = args.heuristic
+    for i in range(len(start)):
+        if start[i] != "B":
+            start[i] = int(start[i])
+    
+    search = Search(type, start,  heuristic)
     search.search()
